630ed2d5410216edfc7d59798c52ba1b841107d9
Dionysis Marinos <marinos@intralot.com>
2016-12-01 17:27:59 +0200

    javascript version with out php

--- Wake on LAN/src/index.js
+++ Wake on LAN/src/index.js
@@ -1,5 +1,6 @@
 var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 var AlexaSkill = require('./AlexaSkill');
+var WoL = require('./wake_on_lan');
 var Computer = function () {
     AlexaSkill.call(this, APP_ID);
 };
@@ -7,7 +8,7 @@ Computer.prototype = Object.create(AlexaSkill.prototype);
 Computer.prototype.constructor = Computer;
 
 Computer.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
@@ -16,16 +17,21 @@ Computer.prototype.eventHandlers.onLaunch = function (launchRequest, session, re
 };
 
 Computer.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
 Computer.prototype.intentHandlers = {
     "WakeOnLan": function (intent, session, response) {
-		// TODO send post to php page
-		console.log("The id to post: "+ intent.sourceIP);
-        response.tell("Wake up.");
+		WoL.wake('20:DE:20:DE:20:DE', {address: '137.99.197.121', port: '9'}, function(error) {
+		if (error) {
+			response.tell("He said, 5 more minutes mom.");
+		} else {
+			response.tell("Wake up.");
+		}
+		});
+        
     },
     "AMAZON.HelpIntent": function (intent, session, response) {
         response.ask("You can ask your computer to wake up.", "Any time.");
